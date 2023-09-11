cbf40b9c00a5c04324dc1d1c9f650a2dfea6a927
Jackson2706 <dung27062002@gmail.com>
2023-09-11 11:24:49 +0700

    Tạo FrameQueue - Đa luồng hóa #7

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import cv2
-import tracker
-from detector import Detector
+import threading
+from FrameCapture import FrameCaptureQueue
 import numpy as np
 import time
 import torch.cuda
 from datetime import datetime
 from law import colorDetector, midPoint, intersect
+import tracker
+from detector import Detector
 
+# Biến hoặc cơ chế đồng bộ hóa chung để thông báo cho các luồng rằng nên kết thúc
+exit_signal = threading.Event()
 
+# Hàng đợi để lưu trữ các frame từ video
+frame_queue = FrameCaptureQueue()
+is_completed = False
 previous = {}
 current = {}
 t_counter1 = []
 v_counter = []
+lane_left = []
+lane_center = [453,174,605,174] 
+lane_right = []
+detector = Detector()
 # Functions
 def updateCrossLight(image, box,time_stamp):
     line_thickness = round(
@@ -52,69 +63,82 @@ def updateCrossLight(image, box,time_stamp):
 
 
 
-# def updateCrossLight(img, x0,y0,x1,y1,track_id, time_stamp):
-#     v_counter.append(track_id)
-#     cv2.rectangle(img, (x0, y0), (x1, y1), (0, 0, 255), -1)
-#     saveDir = "./law_img"
-#     file_name = "vuot_den_{}".format(time_stamp)
-#     cv2.imwrite("{}/{}.jpg".format(saveDir, file_name), img)
+# Hàm để lấy frame từ video và đưa vào hàng đợi
+def video_capture_thread():
+    cap = cv2.VideoCapture("rtsp://admin:Admin@123@27.72.149.50:1554/profile3/media.smp") # open one video
 
-lane_left = []
-lane_center = [453,174,605,174] 
-lane_right = []
-capture = cv2.VideoCapture("rtsp://admin:Admin@123@27.72.149.50:1554/profile3/media.smp") # open one video
-#capture = cv2.VideoCapture("/media/jackson/Extra/AI_Camera_CTARG/video_output.mp4") # open one video
+    while not exit_signal.is_set():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_queue.push(frame)
+        print(frame_queue.count)
 
-detector = Detector()
-i = 0
-while True:
-    torch.cuda.empty_cache()
-    t = time.time()
-    _, frame = capture.read()
-    print(frame.shape)
-    boxes = detector.detect(frame)
-    if len(boxes):
-        boxes = tracker.update(boxes, frame)
+    cap.release()
 
-    current_color = colorDetector(frame)
-    time_stamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
-    for i in range(len(boxes)):
-        box = boxes[i][:4]
-        track_id = boxes[i][-1]
-        cls = boxes[i][4]
-        x0 = int(box[0])
-        y0 = int(box[1])
-        x1 = int(box[2])
-        y1 = int(box[3])
-        current[track_id]  = midPoint(x0,y0,x1,y1)
+# Hàm để hiển thị frame từ hàng đợi
+def display_thread():
+    while not exit_signal.is_set():
+        if not frame_queue.isEnough():
+            continue
+        torch.cuda.empty_cache()
+        t = time.time()
+        frame = frame_queue.pop()
+        print(frame.shape)
+        boxes = detector.detect(frame)
+        if len(boxes):
+            boxes = tracker.update(boxes, frame)
 
-        if track_id in previous:
-            cv2.line(frame, previous[track_id], current[track_id], (0,255,0), 1)
-            line_group0 = [lane_left, lane_center, lane_right]
-            for element in line_group0:
-                if len(element):
-                    # print(element)
-                    start_line = element[0],element[1]
-                    end_line = element[2], element[3]
-                    cv2.line(frame, start_line, end_line, (0,0,255), 2)
-                    if intersect(previous[track_id],current[track_id], start_line, end_line):
-                        print(intersect(previous[track_id],current[track_id], start_line, end_line))
-                        if line_group0.index(element) == 1:
-                            t_counter1.append(track_id)
-                            if current_color != "red":
-                                print("Fined")
-                                updateCrossLight(frame,box=(x0, y0,x1,y1,cls, track_id), time_stamp=time_stamp)
-        previous[track_id] = current[track_id]
+        current_color = colorDetector(frame)
+        time_stamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+        for i in range(len(boxes)):
+            box = boxes[i][:4]
+            track_id = boxes[i][-1]
+            cls = boxes[i][4]
+            x0 = int(box[0])
+            y0 = int(box[1])
+            x1 = int(box[2])
+            y1 = int(box[3])
+            current[track_id]  = midPoint(x0,y0,x1,y1)
 
-    print(colorDetector(frame=frame))
-    frame = tracker.draw_bboxes(frame,boxes, None)
-    cv2.imshow("Video Stream", frame)
+            if track_id in previous:
+                cv2.line(frame, previous[track_id], current[track_id], (0,255,0), 1)
+                line_group0 = [lane_left, lane_center, lane_right]
+                for element in line_group0:
+                    if len(element):
+                        # print(element)
+                        start_line = element[0],element[1]
+                        end_line = element[2], element[3]
+                        cv2.line(frame, start_line, end_line, (0,0,255), 2)
+                        if intersect(previous[track_id],current[track_id], start_line, end_line):
+                            print(intersect(previous[track_id],current[track_id], start_line, end_line))
+                            if line_group0.index(element) == 1:
+                                t_counter1.append(track_id)
+                                if current_color != "red":
+                                    print("Fined")
+                                    updateCrossLight(frame,box=(x0, y0,x1,y1,cls, track_id), time_stamp=time_stamp)
+            previous[track_id] = current[track_id]
 
-# #    ` cv2.imwrite(f"law_img/{i}.jpg", frame)
-#     i += 1`
-    duration = time.time() - t
-    print(f"fps: {1/duration}")
-    if cv2.waitKey(1) == ord('q'):
-        break
+        print(colorDetector(frame=frame))
+        frame = tracker.draw_bboxes(frame,boxes, None)
+        cv2.imshow("Video Stream", frame)
 
-cv2.destroyAllWindows()
+    # #    ` cv2.imwrite(f"law_img/{i}.jpg", frame)
+    #     i += 1`
+        duration = time.time() - t
+        print(f"fps: {1/duration}")
+        if cv2.waitKey(1) == ord('q'):
+            exit_signal.set()
+            break
+
+    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    # Tạo và khởi động các thread
+    capture_thread = threading.Thread(target=video_capture_thread)
+    display_thread = threading.Thread(target=display_thread)
+
+    capture_thread.start()
+    display_thread.start()
+    
+    capture_thread.join()
+    display_thread.join()
